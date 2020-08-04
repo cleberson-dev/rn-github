c92bec6c1a190c7ff7140cc5eca3d326c4ddc2cb
cleberson-dev <cleberson.dev@gmail.com>
2020-08-04 11:49:19 -0300

    Displayed user's image on Details screen

--- screens/Details.js
+++ screens/Details.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, Button, Image } from "react-native";
 
 export default function DetailsScreen({ navigation, route }) {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     fetch(`https://api.github.com/users/${route.params.user}`)
-    .then(res => res.json())
-    .then(json => setUser(json))
-    .catch(console.error);
+      .then((res) => res.json())
+      .then((json) => setUser(json))
+      .catch(console.error);
   }, []);
 
-
-  return (
+  return user ? (
     <View>
-      <Text>Olá, {user.name}</Text>
-      <Button
-        title="Voltar"
-        onPress={() => navigation.goBack()}
+      <Image
+        style={{ height: 64, width: 64 }} 
+        source={{ uri: user.avatar_url || 'https://www.noupe.com/wp-content/uploads/2012/10/octocat.png' }} 
       />
+      <Text>Olá, {user.name || user.login}</Text>
+      <Button title="Voltar" onPress={() => navigation.goBack()} />
     </View>
-  )
+  ) : (
+    <View>
+      <Text>Sem resultados</Text>
+    </View>
+  );
 }
